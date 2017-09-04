094eec9f405017b2993b175508c1bc47a974e5d6
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-04 17:46:54 -0400

    :donE:

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
-  end
+    @student =Student.new(params[:student])
+    end
 
   def create
+        @student = Student.new
+         @student.first_name = params[:first_name]
+         @student.last_name = params[:last_name]
+         @student.save
+         redirect_to students_path(@student) #redirect to the show page for this new student
+         #redirect_to students_path to go to the index page
   end
 
 end
